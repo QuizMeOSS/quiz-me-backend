262fcf4cbf04fa3f86b28fd005299316ac37d477
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2026-01-30 11:04:30 +0200

    add JwtUtil and config

--- src/main/java/com/quizme/config/AppProperties.java
+++ src/main/java/com/quizme/config/AppProperties.java
@@ -14,8 +14,27 @@ public class AppProperties {
 
     public static class Auth {
         private String pepper;
+        private Jwt jwt;
 
         public String getPepper() { return pepper; }
         public void setPepper(String pepper) { this.pepper = pepper; }
+        public Jwt getJwt() { return jwt; }
+        public void setJwt(Jwt jwt) { this.jwt = jwt; }
+    }
+
+    public static class Jwt {
+
+        private String secret;
+        private long accessTokenDuration;
+        private long refreshTokenDuration;
+
+        public String getSecret() { return secret; }
+        public void setSecret(String secret) { this.secret = secret; }
+
+        public long getAccessTokenDuration() { return accessTokenDuration; }
+        public void setAccessTokenDuration(long duration) { this.accessTokenDuration = duration; }
+
+        public long getRefreshTokenDuration() { return refreshTokenDuration; }
+        public void setRefreshTokenDuration(long duration) { this.refreshTokenDuration = duration; }
     }
 }
