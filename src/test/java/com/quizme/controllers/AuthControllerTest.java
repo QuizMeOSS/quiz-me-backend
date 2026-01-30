794a91b29f05c16bb733587a97674731761b2d10
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2026-01-30 15:09:04 +0200

    add login tests to AuthControllerTest

--- src/test/java/com/quizme/controllers/AuthControllerTest.java
+++ src/test/java/com/quizme/controllers/AuthControllerTest.java
@@ -1,14 +1,19 @@
 package com.quizme.controllers;
 
 import com.quizme.config.AppProperties;
+import com.quizme.dto.CredentialsLoginRequestDto;
+import com.quizme.dto.LoginResponseDto;
 import com.quizme.dto.RegisterCredentialsRequestDto;
 import com.quizme.entities.User;
 import com.quizme.mappers.ResultToResponseEntityMapper;
 import com.quizme.security.JwtUtil;
 import com.quizme.services.LoginService;
 import com.quizme.services.RegistrationService;
+import com.quizme.services.result.Failure;
+import com.quizme.services.result.FailureReason;
 import com.quizme.services.result.Result;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
@@ -17,7 +22,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.client.RestTestClient;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(AuthController.class)
@@ -32,7 +40,7 @@ class AuthControllerTest {
     private RegistrationService registrationService;
     @MockitoBean
     private LoginService loginService;
-    @MockitoBean
+    @MockitoBean(answers = Answers.RETURNS_DEEP_STUBS) // avoid mocking Auth() and Jwt() explicitly
     private AppProperties appProperties;
     @MockitoBean
     private JwtUtil jwtUtil;
@@ -57,5 +65,69 @@ class AuthControllerTest {
                     assertEquals("e", user.getResponseBody().getEmail());
                     assertEquals("u", user.getResponseBody().getUsername());
                 });
+    }
+
+    @Test
+    void login_failureIsMappedToApiError() {
+        var requestDto = new CredentialsLoginRequestDto("email", "pw");
+        Result<LoginResponseDto> result = Result.failure(new Failure(FailureReason.NOT_FOUND, "Incorrect login data"));
+        when(loginService.login(requestDto)).thenReturn(result);
+
+        restTestClient.post()
+                .uri("/login")
+                .body(requestDto)
+                .exchange();
+
+        // verify the mapper was invoked to map the response to ApiError
+        verify(mapper).map(result, "/login");
+
+    }
+
+    @Test
+    void login_accessTokenStoredInCookie() {
+        var requestDto = new CredentialsLoginRequestDto("email", "pw");
+        // make access token duration 1 second (1000 millis)
+        when(appProperties.getAuth().getJwt().getAccessTokenDuration()).thenReturn(1000L);
+        var result = Result.success(new LoginResponseDto("access", "refresh"));
+        when(loginService.login(requestDto)).thenReturn(result);
+
+        restTestClient.post()
+                .uri("/login")
+                .body(requestDto)
+                .exchange()
+                .expectCookie()
+                .httpOnly("access_token", true)
+                .expectCookie()
+                .secure("access_token", true)
+                .expectCookie()
+                .path("access_token", "/")
+                .expectCookie()
+                .sameSite("access_token", "Strict")
+                .expectCookie()
+                .maxAge("access_token", Duration.ofSeconds(1));
+    }
+
+    @Test
+    void login_refreshTokenStoredInCookie() {
+        var requestDto = new CredentialsLoginRequestDto("email", "pw");
+        // make refresh token duration 10 seconds (10000 millis)
+        when(appProperties.getAuth().getJwt().getRefreshTokenDuration()).thenReturn(10000L);
+        var result = Result.success(new LoginResponseDto("access", "refresh"));
+        when(loginService.login(requestDto)).thenReturn(result);
+
+        restTestClient.post()
+                .uri("/login")
+                .body(requestDto)
+                .exchange()
+                .expectCookie()
+                .httpOnly("refresh_token", true)
+                .expectCookie()
+                .secure("refresh_token", true)
+                .expectCookie()
+                .path("refresh_token", "/refresh")
+                .expectCookie()
+                .sameSite("refresh_token", "Strict")
+                .expectCookie()
+                .maxAge("refresh_token", Duration.ofSeconds(10));
     }
 }
