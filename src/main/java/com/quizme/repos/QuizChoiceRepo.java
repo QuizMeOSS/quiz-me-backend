7638b73dae1ef859af49fb6d8182c3f58405cc3b
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2026-04-05 14:44:59 +0200

    removed unused entities and repos

--- src/main/java/com/quizme/repos/QuizChoiceRepo.java
+++
@@ -1,8 +0,0 @@
-package com.quizme.repos;
-
-import com.quizme.entities.QuizChoice;
-import com.quizme.entities.QuizChoiceId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizChoiceRepo extends JpaRepository<QuizChoice, QuizChoiceId> {
-}
--- src/main/java/com/quizme/repos/QuizQuestionRepo.java
+++
@@ -1,8 +0,0 @@
-package com.quizme.repos;
-
-import com.quizme.entities.QuizQuestion;
-import com.quizme.entities.QuizQuestionId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizQuestionRepo extends JpaRepository<QuizQuestion, QuizQuestionId> {
-}
