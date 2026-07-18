0112eda19173dc9518146549e7eda079e147344f
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2026-07-18 12:42:52 +0300

    fix: add key serializer to redis template

--- business/src/main/java/com/quizme/config/RedisConfig.java
+++ business/src/main/java/com/quizme/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import tools.jackson.databind.DefaultTyping;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
@@ -20,6 +21,10 @@ public class RedisConfig {
                                                        ObjectMapper objectMapper) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
+        // key serializer is needed otherwise instead of e.g. "idempotency:1"
+        // the key will be serialized as Java object bytes, not plain text
+        // and we end up with a key like: "\xac\xed\x00\x05t\x00\x10idempotency:1"
+        template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJacksonJsonRedisSerializer(objectMapper));
         return template;
     }
