2055c8d506a7267a4688f3ae8424eb4a01b42665
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2026-07-23 10:42:17 +0300

    enable async for quizme app

--- app/src/main/java/com/quizme/QuizmeApplication.java
+++ app/src/main/java/com/quizme/QuizmeApplication.java
@@ -3,9 +3,11 @@ package com.quizme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableDiscoveryClient // for service discovery
+@EnableAsync
 public class QuizmeApplication {
 
     static void main(String[] args) {
