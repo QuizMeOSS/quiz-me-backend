82c2de8ae83eb49762f87033a12f53e3ff36e98a
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2026-07-23 22:54:58 +0300

    add tests that categories are cached

--- business/src/test/java/com/quizme/services/CategoryServiceTest.java
+++ business/src/test/java/com/quizme/services/CategoryServiceTest.java
@@ -9,6 +9,7 @@ import com.quizme.exceptionhandler.result.FailureReason;
 import com.quizme.exceptionhandler.result.Result;
 import com.quizme.repos.CategoryRepo;
 import com.quizme.services.cache.ApiCacheService;
+import com.quizme.services.cache.idempotency.CacheableResults;
 import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,16 +17,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
@@ -117,5 +118,42 @@ class CategoryServiceTest {
         var categories = categoryService.getCategoriesByIdsForUser(user, Set.of());
 
         assertEquals(List.of(expectedCat1, expectedCat2), categories);
+    }
+
+    @Test
+    void getAllCategories_WHEN_cachedCategoriesExist_THEN_noDbQuery() {
+        var user = new User("e", "u");
+        var cat1 = new Category(user.getId(), "Cat1");
+        var cat2 = new Category(user.getId(), "Cat2");
+        var expectedCat1 = new CreatedCategoryDto(0, "Cat1");
+        var expectedCat2 = new CreatedCategoryDto(0, "Cat2");
+        when(apiCacheService.get(CacheableResults.CATEGORY, user.getId().toString(), apiCacheService.createListType(Category.class)))
+                .thenReturn(Optional.of(List.of(cat1, cat2)));
+
+        var categories = categoryService.getAllCategories(user);
+
+        assertEquals(Set.of(expectedCat1, expectedCat2), categories);
+        verifyNoInteractions(categoryRepo);
+    }
+
+    @Test
+    void getCategoriesByIdsForUser_WHEN_cachedCategoriesExist_THEN_noDbQuery() {
+        // arrange
+        var user = new User("e", "u");
+        var cat1 = new Category(user.getId(), "Cat1");
+        var cat2 = new Category(user.getId(), "Cat2");
+        // mock ids being set
+        ReflectionTestUtils.setField(cat1, "id", 1L);
+        ReflectionTestUtils.setField(cat2, "id", 2L);
+        // mock results exist in cache
+        when(apiCacheService.get(CacheableResults.CATEGORY, user.getId().toString(), apiCacheService.createListType(Category.class)))
+                .thenReturn(Optional.of(List.of(cat1, cat2)));
+
+        // act - search for category 1 only
+        var categories = categoryService.getCategoriesByIdsForUser(user, Set.of(1L));
+
+        // assert
+        assertEquals(Set.of(cat1), categories);
+        verifyNoInteractions(categoryRepo);
     }
 }
